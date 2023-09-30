660f184f6f76558423ef70a3fc48c3338557be9a
Damien Delmas <kerdelos@gmail.com>
2023-09-30 23:22:42 +0200

    wip drop and collect coins

--- logic/manager/GameManager.gd
+++ logic/manager/GameManager.gd
@@ -1,21 +1,27 @@
 extends Node
 
+const DEBUG_UNLIMITED_MONEY = true;
 
 
 var elastic: Elastic
 var player;
 var level_manager;
 
-
+var coins : int = 0;
+const COST_POST = 5;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func try_drop_post(drop_position :Vector2):
-	#check if enough money
-	level_manager.drop_post(drop_position);
+	if coins >= COST_POST or DEBUG_UNLIMITED_MONEY:
+		level_manager.drop_post(drop_position);
+		coins = coins - COST_POST;
 
+func collect_coin():
+	coins = coins + 1;
+	print("coin amount : " + str(coins));
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
