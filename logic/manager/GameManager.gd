caad0a0c89655872b3aec0bfcd74d7356fc800c6
Damien Delmas <kerdelos@gmail.com>
2023-09-30 22:34:58 +0200

    drop post

--- logic/manager/GameManager.gd
+++ logic/manager/GameManager.gd
@@ -1,11 +1,20 @@
 extends Node
 
+
+
 var elastic: Elastic
 var player;
+var level_manager;
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func try_drop_post(drop_position :Vector2):
+	#check if enough money
+	level_manager.drop_post(drop_position);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
