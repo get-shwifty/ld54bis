da45fff92b49ba73cc1405c6935ad6aa37e06f35
Damien Delmas <kerdelos@gmail.com>
2023-10-01 20:24:48 +0200

    working on entities shader

--- logic/level/shader_test_scene.gd
+++ logic/level/shader_test_scene.gd
@@ -14,3 +14,9 @@ func send_info_to_shader():
 
 	$ShaderRenderer.get_material().set_shader_parameter("elastic_current_points_number", count);
 	$ShaderRenderer.get_material().set_shader_parameter("elastic_points", elastic_points);
+	
+	for c in $points.get_children():
+		var entity_mat = c.get_material();
+		if entity_mat != null:
+			entity_mat.set_shader_parameter("elastic_current_points_number", count);
+			entity_mat.set_shader_parameter("elastic_points", elastic_points);
